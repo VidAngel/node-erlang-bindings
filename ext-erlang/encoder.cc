edad41f294fc30ded867ed38142d6e51691beb3e
Ian Hunter <ian@ianhunter.me>
2020-01-28 17:10:10 -0600

    Move function definitions to header file

--- ext-erlang/encoder.cc
+++ ext-erlang/encoder.cc
@@ -5,22 +5,6 @@
 #include <cstdio>
 #include <cmath>
 using std::string;
-Napi::Value decode_atom(Napi::Env env, ei_x_buff* term, int* index);
-Napi::Number decode_int(Napi::Env env,ei_x_buff* term, int* index);
-Napi::Number decode_double(Napi::Env env,ei_x_buff* term, int* index);
-Napi::Object decode_map(Napi::Env env,ei_x_buff* term, int* index, int arity);
-Napi::Object decode_tuple(Napi::Env env, ei_x_buff* term, int* index, int arity);
-Napi::Array decode_list(Napi::Env env, ei_x_buff* term, int* index, int arity);
-Napi::Value decode_binary(Napi::Env env, ei_x_buff* term, int* index, int size);
-Napi::String decode_charlist(Napi::Env env, ei_x_buff* term, int* index, int size);
-
-void encode_string(Napi::Env env, Napi::Value val, ei_x_buff* request);
-void encode_atom(Napi::Env env, Napi::Value val, ei_x_buff* request);
-void encode_tuple(Napi::Env env, Napi::Value val, ei_x_buff* request);
-void encode_number(Napi::Env env, Napi::Value val, ei_x_buff* request);
-void encode_object(Napi::Env env, Napi::Value val, ei_x_buff* request);
-void encode_bool(Napi::Env env, Napi::Value val, ei_x_buff* request);
-void encode_array(Napi::Env env, Napi::Value val, ei_x_buff* request);
 
 string to_string(Napi::Env env, Napi::Value val);
 
--- ext-erlang/encoder.h
+++ ext-erlang/encoder.h
@@ -2,8 +2,24 @@
 #include <napi.h>
 #include <erl_interface.h>
 #include <ei.h>
+Napi::Value decode_atom(Napi::Env env, ei_x_buff* term, int* index);
+Napi::Number decode_int(Napi::Env env,ei_x_buff* term, int* index);
+Napi::Number decode_double(Napi::Env env,ei_x_buff* term, int* index);
+Napi::Object decode_map(Napi::Env env,ei_x_buff* term, int* index, int arity);
+Napi::Object decode_tuple(Napi::Env env, ei_x_buff* term, int* index, int arity);
+Napi::Array decode_list(Napi::Env env, ei_x_buff* term, int* index, int arity);
+Napi::Value decode_binary(Napi::Env env, ei_x_buff* term, int* index, int size);
+Napi::String decode_charlist(Napi::Env env, ei_x_buff* term, int* index, int size);
 Napi::Value decode_erlang(Napi::Env env, ei_x_buff* term, int* index);
 Napi::Value decode_erlang(Napi::Env env, ei_x_buff* term);
+
+void encode_string(Napi::Env env, Napi::Value val, ei_x_buff* request);
+void encode_atom(Napi::Env env, Napi::Value val, ei_x_buff* request);
+void encode_tuple(Napi::Env env, Napi::Value val, ei_x_buff* request);
+void encode_number(Napi::Env env, Napi::Value val, ei_x_buff* request);
+void encode_object(Napi::Env env, Napi::Value val, ei_x_buff* request);
+void encode_bool(Napi::Env env, Napi::Value val, ei_x_buff* request);
+void encode_array(Napi::Env env, Napi::Value val, ei_x_buff* request);
 void encode_value(Napi::Env env, Napi::Value val, ei_x_buff* request);
 
 bool is_tuple(Napi::Value val);
